93ecc4f94030a2c4f93ed558e36e958b0f4b1b44
shashank-shark <shashankreddy231@gmail.com>
2021-02-27 16:41:52 +0530

    Need to fix GatheringByteChannel over write bug.

--- src/ScatterGatherChannels/ContScatterGatterReadWriteFile.java
+++ src/ScatterGatherChannels/ContScatterGatterReadWriteFile.java
@@ -5,6 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 import java.nio.channels.Channels;
 import java.nio.channels.GatheringByteChannel;
 import java.nio.channels.ScatteringByteChannel;
@@ -18,29 +19,23 @@ public class ContScatterGatterReadWriteFile {
         FileInputStream fileInputStream = new FileInputStream(inputFile);
         sourceOfByteScatter = (ScatteringByteChannel) Channels.newChannel(fileInputStream);
 
-        long totalNumberOfBytesToScatter = inputFile.length();
-
         // create 4 ByteBuffers of each 2048 bytes
-        ByteBuffer[] byteBuffers = new ByteBuffer[4];
-        for(int i = 0; i < 4; i ++) {
-            byteBuffers[i] = ByteBuffer.allocateDirect(2048);
-        }
-
+        ByteBuffer appOneBuffer = ByteBuffer.allocateDirect(2048);
+        ByteBuffer appTwoBuffer = ByteBuffer.allocateDirect(4096);
+        ByteBuffer[] byteBuffers = { appOneBuffer, appTwoBuffer };
 
         GatheringByteChannel destinationForByteScatter;
         File outputFile = new File("data/newScatterFile.txt");
         if (! outputFile.exists()) outputFile.createNewFile();
-        FileOutputStream fileOutputStream = new FileOutputStream(outputFile);
+        FileOutputStream fileOutputStream = new FileOutputStream(outputFile, true);
         destinationForByteScatter = (GatheringByteChannel) Channels.newChannel(fileOutputStream);
 
-        long numberOfBytesRead = 0L;
-        do {
-            numberOfBytesRead += sourceOfByteScatter.read(byteBuffers);
+        while (sourceOfByteScatter.isOpen() && sourceOfByteScatter.read(byteBuffers) != -1) {
+            byteBuffers[0] = appOneBuffer;
+            byteBuffers[1] = appTwoBuffer;
             destinationForByteScatter.write(byteBuffers);
-            for (int i = 0; i < 4; i ++) {
-                byteBuffers[i].clear();
-            }
-        } while (numberOfBytesRead < totalNumberOfBytesToScatter);
+            byteBuffers[0].clear(); byteBuffers[1].clear();
+        }
 
     }
 }
