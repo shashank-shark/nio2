9a1bee1521b113c083b70dbf38c77edd336480b1
shashank-shark <shashankreddy231@gmail.com>
2021-03-03 21:42:03 +0530

    Attachment model for AsynchronousSocketChannel.

---
+++ src/async/AsyncServer.java
@@ -0,0 +1,22 @@
+package async;
+
+import java.io.IOException;
+import java.nio.channels.AsynchronousServerSocketChannel;
+
+public class AsyncServer {
+
+    private static final int PORT = 9999;
+    private static final String HOST = "localhost";
+
+    public static void main (String[] args) {
+
+        AsynchronousServerSocketChannel channelServer = null;
+        try {
+            channelServer = AsynchronousServerSocketChannel.open();
+        } catch (IOException ioException) {
+            System.out.println("Exception caught: " + ioException.getMessage());
+        }
+
+
+    }
+}
